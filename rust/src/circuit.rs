0b83fdc9bd27cf9458980dc5e3b4a31700c3840f
kpzip <kpzipgames@gmail.com>
2024-02-17 13:19:11 -0800

    work on rust implementation

--- rust/src/circuit.rs
+++ rust/src/circuit.rs
@@ -6,21 +6,19 @@ pub struct Circuit<'a> {
 
 impl<'a> Circuit<'a> {
     pub fn new() -> Circuit<'a> {
-        let ground: ConnectionPoint = ConnectionPoint {
-            id: 0,
-            voltage: 0.0,
-            circuit: None,
-        };
-
         let mut circuit: Circuit = Circuit {
             connection_point_counter: 0,
             connection_points: Vec::new(),
             ground: None,
         };
 
-        circuit.connection_points.push(ground);
+        let ground: ConnectionPoint = ConnectionPoint {
+            id: 0,
+            voltage: 0.0,
+            circuit: &circuit,
+        };
 
-        circuit.ground = circuit.connection_points.first();
+        circuit.connection_points.push(ground);
 
         circuit
     }
@@ -29,7 +27,7 @@ impl<'a> Circuit<'a> {
 pub struct ConnectionPoint<'a> {
     id: u32,
     voltage: f64,
-    circuit: Option<&'a Circuit<'a>>,
+    circuit: &'a Circuit<'a>,
 }
 
 impl<'a> ConnectionPoint<'a> {
@@ -37,7 +35,7 @@ impl<'a> ConnectionPoint<'a> {
         ConnectionPoint {
             id: circuit.connection_point_counter,
             voltage: 0.0,
-            circuit: Some(circuit),
+            circuit: circuit,
         }
     }
 }
